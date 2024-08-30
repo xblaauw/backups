057d9a2a28db3a419c9f9ffd701dd1dc3fd3299b
Xander <xanderblaauw@gmail.com>
2024-08-30 19:38:26 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-# The goal of this file is to setup the systemd service using the backup.service file and to make an initial backup.
-# This script takes 3 arguments, `source` and `destination1` and `destination2`, all arguments are a valid filesystem path.
-# Backups are made using Rsync to make timestamped incremental backups of the `source` directory.
+# The goal of this file is to setup the systemd service using the `backup1.service` and `backup2.service` files and to make an initial backup of `source` in both locations.
+# This script uses the information in .env to determine what goes where.
+# Backups are made using Rsync to make timestamped incremental backups of the `source` directory into `destination1` and `destination2`.
