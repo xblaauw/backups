eb2b669fa34876e8c4972a51b281ea2acb3cc197
Xander <xanderblaauw@gmail.com>
2024-08-30 19:28:22 +0200

    added

--- prompt.md
+++ prompt.md
@@ -1,1 +1,2 @@
-We want to use Rsync to make incremental backups of the `source` directory and store all relevant data into the `destination` directory.
+We want to use Rsync to make incremental backups of the `source` directory and store all relevant data into the `destination1` and `destination2` directories.
+There should be an easily configurable option to run these backups at different times.
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # The goal of this file is to setup the systemd service using the backup.service file and to make an initial backup.
-# This script takes 2 arguments, `source` and `destination`, both arguments are a valid filesystem path.
-# Backups are made using Rsync to make timestamped versioned backups of the `source` directory.
+# This script takes 3 arguments, `source` and `destination1` and `destination2`, all arguments are a valid filesystem path.
+# Backups are made using Rsync to make timestamped incremental backups of the `source` directory.
